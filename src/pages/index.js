8a92bce3ec6b4ef6c977c5da47b57a257cdce7ba
tharunkalluru <tharun.kalluru@contentstack.com>
2025-01-23 17:49:44 -0500

    Added Lytics Code

--- src/pages/index.js
+++ src/pages/index.js
@@ -70,7 +70,7 @@ const Home = ({ host, apiKey }) => {
       .catch(error => {
         console.error('Error fetching data:', error)
       })
-  })
+  }, [])
 
   if (!host || !apiKey) return <div>{t('connexionFailed')}</div>
 
@@ -80,7 +80,23 @@ const Home = ({ host, apiKey }) => {
         value={{ selectedLanguage, setSelectedLanguage: setSelectedCountry }}
       >
         <Head>
-          {/* <!-- Google tag (gtag.js) --> */}
+          {/* Lytics Tracking Script */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(){"use strict";var o=window.jstag||(window.jstag={}),r=[];function n(e){o[e]=function(){for(var n=arguments.length,t=new Array(n),i=0;i<n;i++)t[i]=arguments[i];r.push([e,t])}}n("send"),n("mock"),n("identify"),n("pageView"),n("unblock"),n("getid"),n("setid"),n("loadEntity"),n("getEntity"),n("on"),n("once"),n("call"),o.loadScript=function(n,t,i){var e=document.createElement("script");e.async=!0,e.src=n,e.onload=t,e.onerror=i;var o=document.getElementsByTagName("script")[0],r=o&&o.parentNode||document.head||document.body,c=o||r.lastChild;return null!=c?r.insertBefore(e,c):r.appendChild(e),this},o.init=function n(t){return this.config=t,this.loadScript(t.src,function(){if(o.init===n)throw new Error("Load error!");o.init(o.config),function(){for(var n=0;n<r.length;n++){var t=r[n][0],i=r[n][1];o[t].apply(o,i)}r=void 0}()}),this}}();
+                jstag.init({
+                  src: 'https://c.lytics.io/api/tag/a84fef4e65fe894eecb707074a47c0f2/latest.min.js',
+                  pageAnalysis: {
+                    dataLayerPull: { disabled: true }
+                  }
+                });
+                jstag.pageView();
+              `,
+            }}
+          />
+
+          {/* Google Tag Manager */}
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-6JLS9M98HW"
@@ -95,6 +111,7 @@ const Home = ({ host, apiKey }) => {
               `,
             }}
           />
+
           <title>{t('Meilisearch starter')}</title>
           <meta name="description" content={t('meta.description')} />
         </Head>
@@ -129,7 +146,6 @@ export const getStaticProps = async ({ locale }) => {
     props: {
       host: MEILISEARCH_URL,
       apiKey: MEILISEARCH_SEARCH_KEY,
-
       ...(await serverSideTranslations(locale, ['common'])),
     },
   }
