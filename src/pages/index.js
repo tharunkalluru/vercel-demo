bb02f8523ab24ad72024b85f69826c1710610f89
tharunkalluru <tharun.kalluru@contentstack.com>
2025-01-23 17:57:35 -0500

    Removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -80,38 +80,6 @@ const Home = ({ host, apiKey }) => {
         value={{ selectedLanguage, setSelectedLanguage: setSelectedCountry }}
       >
         <Head>
-          {/* Lytics Tracking Script */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                !function(){"use strict";var o=window.jstag||(window.jstag={}),r=[];function n(e){o[e]=function(){for(var n=arguments.length,t=new Array(n),i=0;i<n;i++)t[i]=arguments[i];r.push([e,t])}}n("send"),n("mock"),n("identify"),n("pageView"),n("unblock"),n("getid"),n("setid"),n("loadEntity"),n("getEntity"),n("on"),n("once"),n("call"),o.loadScript=function(n,t,i){var e=document.createElement("script");e.async=!0,e.src=n,e.onload=t,e.onerror=i;var o=document.getElementsByTagName("script")[0],r=o&&o.parentNode||document.head||document.body,c=o||r.lastChild;return null!=c?r.insertBefore(e,c):r.appendChild(e),this},o.init=function n(t){return this.config=t,this.loadScript(t.src,function(){if(o.init===n)throw new Error("Load error!");o.init(o.config),function(){for(var n=0;n<r.length;n++){var t=r[n][0],i=r[n][1];o[t].apply(o,i)}r=void 0}()}),this}}();
-                jstag.init({
-                  src: 'https://c.lytics.io/api/tag/a84fef4e65fe894eecb707074a47c0f2/latest.min.js',
-                  pageAnalysis: {
-                    dataLayerPull: { disabled: true }
-                  }
-                });
-                jstag.pageView();
-              `,
-            }}
-          />
-
-          {/* Google Tag Manager */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-6JLS9M98HW"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-6JLS9M98HW');
-              `,
-            }}
-          />
-
           <title>{t('Meilisearch starter')}</title>
           <meta name="description" content={t('meta.description')} />
         </Head>
